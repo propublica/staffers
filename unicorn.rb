05ce420a91ee87296ba36c48cf7cabaa8abe6ece
Eric Mill <eric@konklone.com>
2014-04-28 18:16:48 -0400

    ignoring unicorn.rb

--- unicorn.rb
+++
@@ -1,7 +0,0 @@
-worker_processes 4 # this should be >= nr_cpus
-pid "/home/eric/sunlight/staffers/unicorn.pid"
-
-working_directory "/home/eric/sunlight/staffers"
-
-# stdout_path "/dev/null"
-# stderr_path "/dev/null"
